6ddf4596504fc08c9369c86e3008735af0ddd719
Galen Elias <gelias@gmail.com>
2023-12-02 09:09:42 -0800

    Day 2 in Rust

---
+++ src/day2/mod.rs
@@ -0,0 +1,76 @@
+use itertools::Itertools;
+
+struct Pull {
+	green: u32,
+	red: u32,
+	blue: u32,
+}
+
+struct Game {
+	id: u32,
+	pulls: Vec<Pull>,
+}
+
+pub fn solve(inputs: Vec<String>) {
+	let games = inputs
+		.iter()
+		.map(|line| {
+			let parts = line.split(": ").collect_vec();
+			let game_id = parts[0].split(" ").collect_vec()[1].parse::<u32>().unwrap();
+
+			let pulls = parts[1]
+				.split("; ")
+				.map(|pull| {
+					let cubes = pull.split(", ").collect_vec();
+
+					let mut pull = Pull {
+						green: 0,
+						red: 0,
+						blue: 0,
+					};
+
+					for cube in cubes {
+						let cube_parts = cube.split(" ").collect_vec();
+						let amount = cube_parts[0].parse::<u32>().unwrap();
+
+						match cube_parts[1] {
+							"green" => pull.green = amount,
+							"red" => pull.red = amount,
+							"blue" => pull.blue = amount,
+							_ => panic!("Unknown color"),
+						}
+					}
+
+					pull
+				})
+				.collect_vec();
+
+			Game { id: game_id, pulls }
+		})
+		.collect_vec();
+
+	let part1 = games
+		.iter()
+		.filter(|game| {
+			game.pulls
+				.iter()
+				.all(|pull| pull.red <= 12 && pull.green <= 13 && pull.blue <= 14)
+		})
+		.map(|game| game.id)
+		.sum::<u32>();
+
+	println!("Part 1: {}", part1);
+
+	let powers = games
+		.iter()
+		.map(|game| {
+			let max_red = game.pulls.iter().map(|pull| pull.red).max().unwrap();
+			let max_green = game.pulls.iter().map(|pull| pull.green).max().unwrap();
+			let max_blue = game.pulls.iter().map(|pull| pull.blue).max().unwrap();
+
+			max_red * max_green * max_blue
+		})
+		.collect_vec();
+
+	println!("Part 2: {}", powers.iter().sum::<u32>());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
 // #[macro_use] extern crate lazy_static;
 extern crate clap;
-extern crate itertools;
-extern crate regex;
-extern crate num;
 extern crate emergence;
+extern crate itertools;
+extern crate num;
+extern crate regex;
 
 use clap::Parser;
 use std::io::{self, BufRead};
 
 mod day1;
-
+mod day2;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-   /// Reads puzzle input from the specified file
-   #[arg(short, long)]
-   file: Option<String>,
+	/// Reads puzzle input from the specified file
+	#[arg(short, long)]
+	file: Option<String>,
 
-   /// Reads puzzle input from standard in
-   #[arg(short, long)]
-   stdin: bool,
+	/// Reads puzzle input from standard in
+	#[arg(short, long)]
+	stdin: bool,
 
-   /// Specifies which day's challenge to run
-   day: u32,
+	/// Specifies which day's challenge to run
+	day: u32,
 }
 
 fn main() {
@@ -35,18 +35,27 @@ fn main() {
 		input = contents.lines().map(String::from).collect();
 	} else if cli.stdin {
 		let stdin = io::stdin();
-		input = stdin.lock().lines()
+		input = stdin
+			.lock()
+			.lines()
 			.filter_map(|l| l.ok())
 			.collect::<Vec<String>>();
 	} else {
 		let aoc_fetcher = emergence::AoC::new(2023).expect("Couldn't instantiate AoC object");
-		let prob_input = aoc_fetcher.read_or_fetch(cli.day as usize).expect("Couldn't fetch problem input");
-		input = prob_input.trim_end_matches('\n').split('\n').map(String::from).collect::<Vec<String>>();
+		let prob_input = aoc_fetcher
+			.read_or_fetch(cli.day as usize)
+			.expect("Couldn't fetch problem input");
+		input = prob_input
+			.trim_end_matches('\n')
+			.split('\n')
+			.map(String::from)
+			.collect::<Vec<String>>();
 	}
 
 	match cli.day {
 		1 => day1::solve(input),
+		2 => day2::solve(input),
 
-		_ => println!("Oops! Day {} isn't implemented yet!", cli.day)
+		_ => println!("Oops! Day {} isn't implemented yet!", cli.day),
 	}
 }
