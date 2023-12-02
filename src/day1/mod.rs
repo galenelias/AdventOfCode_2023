d9541d112c817c5c5c4ffd086ea5d45a46f1495f
Galen Elias <gelias@gmail.com>
2023-12-02 09:09:42 -0800

    Cargo fmt

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,7 +1,9 @@
 use itertools::Itertools;
 
 fn str_to_digit(s: &str, include_strings: bool) -> Option<u32> {
-	let number_strings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+	let number_strings = [
+		"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+	];
 
 	if let Some(digit) = s.chars().next().unwrap().to_digit(10) {
 		Some(digit)
@@ -19,17 +21,19 @@ fn str_to_digit(s: &str, include_strings: bool) -> Option<u32> {
 
 pub fn solve(inputs: Vec<String>) {
 	for part in 0..2 {
-		let digits = inputs.iter()
-			.map(|line|  {
+		let digits = inputs
+			.iter()
+			.map(|line| {
 				let line_len = line.len();
 				(0..line_len)
 					.map(|i| &line[i..])
-					.filter_map(|substr| str_to_digit(substr, /*include_strings=*/part == 1))
+					.filter_map(|substr| str_to_digit(substr, /*include_strings=*/ part == 1))
 					.collect_vec()
 			})
 			.collect_vec();
 
-		let numbers = digits.iter()
+		let numbers = digits
+			.iter()
 			.map(|digits| digits.first().unwrap() * 10 + digits.last().unwrap())
 			.collect_vec();
 
