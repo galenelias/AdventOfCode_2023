d90a50bfc5e7e10278230ef85dc1aae2ba038f0e
Galen Elias <gelias@gmail.com>
2023-12-03 14:16:13 -0800

    Day 3 (clean)

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,62 +1,46 @@
 use itertools::Itertools;
-use std::collections::{HashSet, VecDeque};
 
-type Grid = Vec<Vec<char>>;
-
-fn get_adjacent_points((r, c): (usize, usize), grid: &Grid) -> Vec<(usize, usize)> {
-	let mut result = Vec::new();
-	if r > 0 && c > 0 {
-		result.push((r - 1, c - 1));
-	}
-	if r > 0 {
-		result.push((r - 1, c));
-	}
-	if r > 0 && c < grid[r - 1].len() - 1 {
-		result.push((r - 1, c + 1));
-	}
-	if c > 0 {
-		result.push((r, c - 1));
-	}
-	if c < grid[r].len() - 1 {
-		result.push((r, c + 1));
-	}
-	if r < grid.len() - 1 && c > 0 {
-		result.push((r + 1, c - 1));
-	}
-	if r < grid.len() - 1 {
-		result.push((r + 1, c));
-	}
-	if r < grid.len() - 1 && c < grid[r + 1].len() - 1 {
-		result.push((r + 1, c + 1));
-	}
-	return result;
+#[derive(Debug, PartialEq)]
+enum NodeType {
+	Number(u64),
+	Symbol(char),
 }
 
-fn adjacent_to_symbol(grid: &Grid, r: usize, c: usize) -> bool {
-	let mut visited = HashSet::new();
-	let mut q: VecDeque<(usize, usize)> = VecDeque::new();
-	q.push_back((r, c));
+#[derive(Debug)]
+struct Node {
+	node_type: NodeType,
+	r: usize,
+	c: usize,
+	width: usize,
+	adjacent_nodes: Vec<usize>,
+}
 
-	while !q.is_empty() {
-		let (r, c) = q.pop_front().unwrap();
-		let ch = grid[r][c];
-
-		if !visited.insert((r, c)) {
-			continue;
-		}
-
-		if !ch.is_ascii_digit() && ch != '.' {
-			return true;
-		}
-
-		for adjacent in get_adjacent_points((r, c), grid) {
-			if grid[adjacent.0][adjacent.1] != '.' {
-				q.push_back(adjacent);
-			}
+impl Node {
+	fn is_other_node_type(&self, other: &Node) -> bool {
+		match (&self.node_type, &other.node_type) {
+			(NodeType::Number(_), NodeType::Symbol(_)) => true,
+			(NodeType::Symbol(_), NodeType::Number(_)) => true,
+			_ => false,
 		}
 	}
 
-	return false;
+	fn is_adjacent(&self, other: &Node) -> bool {
+		let dr = if self.r >= other.r {
+			self.r - other.r
+		} else {
+			other.r - self.r
+		};
+
+		let dc = if other.c >= self.c + self.width - 1 {
+			other.c - (self.c + self.width - 1)
+		} else if self.c >= other.c + other.width - 1 {
+			self.c - (other.c + other.width - 1)
+		} else {
+			0
+		};
+
+		return dr <= 1 && dc <= 1;
+	}
 }
 
 pub fn solve(inputs: Vec<String>) {
@@ -65,64 +49,78 @@ pub fn solve(inputs: Vec<String>) {
 		.map(|line| line.chars().collect_vec())
 		.collect_vec();
 
-	let mut part1 = 0;
+	let mut nodes = Vec::new();
 
 	for r in 0..grid.len() {
 		for c in 0..grid[r].len() {
-			if grid[r][c].is_ascii_digit() && (c == 0 || !grid[r][c - 1].is_ascii_digit()) {
-				// Start of word, check if adjacent to symbol
-				if adjacent_to_symbol(&grid, r, c) {
-					part1 += grid[r][c..]
+			if grid[r][c].is_ascii_digit() {
+				// Check for start of word
+				if c == 0 || !grid[r][c - 1].is_ascii_digit() {
+					let number_str = grid[r][c..]
 						.iter()
 						.take_while(|c| c.is_ascii_digit())
-						.collect::<String>()
-						.parse::<u32>()
-						.unwrap();
+						.collect::<String>();
+
+					let number = number_str.parse::<u64>().unwrap();
+
+					nodes.push(Node {
+						r,
+						c,
+						node_type: NodeType::Number(number),
+						width: number_str.len(),
+						adjacent_nodes: Vec::new(),
+					});
 				}
+			} else if grid[r][c] == '.' {
+				continue;
+			} else {
+				nodes.push(Node {
+					r,
+					c,
+					node_type: NodeType::Symbol(grid[r][c]),
+					width: 1,
+					adjacent_nodes: Vec::new(),
+				});
 			}
 		}
 	}
 
-	println!("Part 1: {}", part1);
-
-	let mut part2: u64 = 0;
-
-	for r in 0..grid.len() {
-		for c in 0..grid[r].len() {
-			if grid[r][c] != '*' {
+	// Calculate node adjacency
+	let nodes_len = nodes.len();
+	for i in 0..nodes_len {
+		for j in 0..nodes_len {
+			if i == j {
 				continue;
 			}
 
-			println!("Checking {}, {}", r, c);
-			let mut adjacent_numbers = Vec::<(usize, usize)>::new();
-			for adjacent in get_adjacent_points((r, c), &grid) {
-				if grid[adjacent.0][adjacent.1].is_ascii_digit() {
-					if adjacent.1 == c - 1 || !grid[adjacent.0][adjacent.1 - 1].is_ascii_digit() {
-						adjacent_numbers.push(adjacent);
-					}
-				}
-			}
-
-			println!("Adjacent numbers: {:?}", adjacent_numbers);
-			if adjacent_numbers.len() == 2 {
-				println!("Gear at {}, {}", r, c);
-				let mut product: u64 = 1;
-				for num in adjacent_numbers {
-					let mut num_start = num.1;
-					while num_start > 0 && grid[num.0][num_start - 1].is_ascii_digit() {
-						num_start -= 1;
-					}
-					product *= grid[num.0][num_start..]
-						.iter()
-						.take_while(|c| c.is_ascii_digit())
-						.collect::<String>()
-						.parse::<u64>()
-						.unwrap();
-				}
-				part2 += product;
+			if nodes[i].is_other_node_type(&nodes[j]) && nodes[i].is_adjacent(&nodes[j]) {
+				nodes[i].adjacent_nodes.push(j);
 			}
 		}
 	}
 
+	let part1 = nodes
+		.iter()
+		.filter(|node| !node.adjacent_nodes.is_empty())
+		.filter_map(|node| match node.node_type {
+			NodeType::Number(value) => Some(value),
+			NodeType::Symbol(_) => None,
+		})
+		.sum::<u64>();
+	println!("Part 1: {}", part1);
+
+	let part2 = nodes
+		.iter()
+		.filter(|node| node.node_type == NodeType::Symbol('*') && node.adjacent_nodes.len() == 2)
+		.filter_map(|node| {
+			match (
+				&nodes[node.adjacent_nodes[0]].node_type,
+				&nodes[node.adjacent_nodes[1]].node_type,
+			) {
+				(NodeType::Number(num1), NodeType::Number(num2)) => Some(num1 * num2),
+				_ => None,
+			}
+		})
+		.sum::<u64>();
 	println!("Part 2: {}", part2);
 }
